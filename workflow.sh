fc1176a738e6e23f703105ee4eb027d6dd012350
Neil Dey <neild799@gmail.com>
2025-08-09 10:03:26 -0700

    Update workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -9,7 +9,7 @@ python3 final_code/ramdas_comparison.py 1 1
 
 python3 final_code/ramdas_comparison_closedform.py
 
-# Code for Example 1 (Example 7 in ArXiv verison)
+# Code for Example 1
 python3 final_code/kmeans.py
 
 # Code for Example 2
@@ -17,6 +17,8 @@ python3 final_code/svm.py
 
 # Code for Exmaple 3
 python3 final_code/lr_condition.py
+python3 final_code/lr_samplesize.py 0
+python3 final_code/lr_powewr.py
 
 # Code for Example 4
 python3 final_code/bestcase.py
@@ -36,18 +38,18 @@ python3 final_code/real_quantile.py True
 python3 final_code/real_quantile.py False
 
 
-# Code for Figure S.1
+# Code for Figure S.1 (Figure 12 in the ArXiv version)
 for i in {1..99}
 do
     python3 final_code/rsafe.py $i
 done
 
-# Code for Example S.1 (Example 5 in the ArXiv version)
+# Code for Example S.1 (Example 7 in the ArXiv version)
 python3 final_code/heavy_tail.py
 
-# Code for Example S2. (Example 6 in the ArXiv version)
+# Code for Example S2. (Example 8 in the ArXiv version)
 python3 final_code/quantile.py
 
-# Code for Example S.3
+# Code for Example S.3 (Example 9 in the ArXiv version)
 python3 final_code/anova.py
 python3 final_code/ci_width.py
